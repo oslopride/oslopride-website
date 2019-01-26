ffe36355e0425e82bdb7fe4dbc6bd00fe530091e
Boye Borg <boye.borg@icloud.com>
2019-01-26 18:03:58 +0100

    fighting against bash

--- scripts/alias-now.sh
+++ scripts/alias-now.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
 
-if [ "$TRAVIS_BRANCH" = "master" ]; then
+if [ "${TRAVIS_BRANCH}" = "master" ]; then
+	echo "${TRAVIS_BRANCH}";
 	now alias set $NOW_DEPLOYMENT oslopride.now.sh --token=$NOW_TOKEN --team oslopride;
 fi
