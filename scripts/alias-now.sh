2cedb389dc2a3b3e968fca0b6c8fe2af264ce22a
Boye Borg <boye.borg@icloud.com>
2019-01-26 18:13:00 +0100

    typo

--- scripts/alias-now.sh
+++ scripts/alias-now.sh
@@ -1,6 +1,5 @@
 #!/bin/bash
 
-if [ "${TRAVIS_BRANCH}" = "master" ]; then
-	echo "${TRAVIS_BRANCH}";
+if [ "${TRAVIS_BRANCH}" = "master" ] && [ "${TRAVIS_PULL_REQUEST}" != "false" ]; then
 	now alias set $NOW_DEPLOYMENT oslopride.now.sh --token=$NOW_TOKEN --team oslopride;
 fi
--- scripts/deploy-now.sh
+++ scripts/deploy-now.sh
@@ -1,3 +1,4 @@
 #!/bin/bash
 
-NOW_DEPLOYMENT=$(now --token=$NOW_TOKEN --team oslopride --no-clipboard)
+export NOW_DEPLOYMENT=$(now --token=$NOW_TOKEN --team oslopride --no-clipboard)
+echo $NOW_DEPLOYMENT
