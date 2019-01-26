9cf83e7cd8615ed2cc80f61094ba609d2fc4a948
Boye Borg <boye.borg@icloud.com>
2019-01-26 17:19:33 +0100

    move pridebot comment to seperate file

---
+++ pridebot-commnet.sh
@@ -0,0 +1,1 @@
+curl -H "Authorization: token ${PRIDEBOT_TOKEN}" -X POST -d "{\"body\": \"${NOW_DEPLOYMENT}\"}" "https://api.github.com/repos/${TRAVIS_REPO_SLUG}/issues/${TRAVIS_PULL_REQUEST}/comments"
