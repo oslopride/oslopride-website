a13dbe72c7ed5aa25331be40ac5297dd5359a782
Boye Borg <boye.borg@icloud.com>
2019-02-18 21:38:18 +0100

    always use production dataset

--- store/api.js
+++ store/api.js
@@ -4,6 +4,6 @@ const isProduction = process.env.NODE_ENV === "production";
 
 export default sanityClient({
   projectId: "2ger3rla",
-  dataset: isProduction ? "production" : "production",
+  dataset: "production",
   useCdn: isProduction
 });
