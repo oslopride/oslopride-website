d6511547bcef3b7f7d5df0cc89d3ff39ef2fb8ad
Boye Borg <boye.borg@icloud.com>
2019-02-18 21:02:41 +0100

    use sanity CDN in prod

--- store/api.js
+++ store/api.js
@@ -4,5 +4,6 @@ const isProduction = process.env.NODE_ENV === "production";
 
 export default sanityClient({
   projectId: "2ger3rla",
-  dataset: isProduction ? "production" : "production"
+  dataset: isProduction ? "production" : "production",
+  useCdn: isProduction
 });
