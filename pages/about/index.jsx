48338f8fddea1eabaafb4402b4b473f71ee1702e
Boye Borg <boye.borg@icloud.com>
2019-02-18 20:59:53 +0100

    display about content on about page

--- pages/about/index.jsx
+++ pages/about/index.jsx
@@ -1,15 +1,30 @@
+import Sheet from "@/components/Sheet";
 import { aboutActions } from "@/store/about";
+import blocksToHtml from "@sanity/block-content-to-html";
 import React, { useEffect } from "react";
 import { connect } from "react-redux";
 
 const About = props => {
+  const { about } = props;
   useEffect(() => {
     props.fetchAboutContent();
   }, []);
-  if (props.about.status !== "SUCCESS") {
-    return <div>Loading ...</div>;
+  if (about.status !== "SUCCESS") {
+    // TODO: Make a better UX while loading
+    return <div>Laster ...</div>;
   }
-  return <div>Om Oss</div>;
+
+  // TODO: Investigate if it's posible to use a react version of this using
+  // react hyperscript: https://github.com/mlmorg/react-hyperscript instead of
+  // the regular hyperscript used here
+  const content = blocksToHtml({ blocks: about.data.body });
+  // eslint-disable-next-line react/no-danger
+  return (
+    <Sheet>
+      <h1>Om Oss</h1>
+      <article dangerouslySetInnerHTML={{ __html: content }} />
+    </Sheet>
+  );
 };
 
 const mapStateToProps = state => ({
