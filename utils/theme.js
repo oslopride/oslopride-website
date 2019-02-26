5b1a953848c8f64580e035f053a162ea27eb1243
Boye Borg <boye.borg@icloud.com>
2019-02-26 19:12:00 +0100

    create theme

---
+++ utils/theme.js
@@ -0,0 +1,16 @@
+import { rgb } from "polished";
+
+const colors = {
+  red: rgb(193, 23, 44),
+  orange: rgb(239, 82, 27),
+  yellow: rgb(255, 193, 0),
+  green: rgb(66, 140, 99),
+  blue: rgb(51, 80, 185),
+  purple: rgb(57, 27, 123),
+  pink: rgb(227, 80, 161)
+};
+
+colors.main = colors.purple;
+colors.accent = colors.punk;
+
+export default colors;
