9cff8f26a12c39138b9235d12be25b12607b5e44
Siddise Hirpa <siddisehirpa@gmail.com>
2019-03-12 19:06:55 +0100

    Change style of partner item

--- pages/partners/index.jsx
+++ pages/partners/index.jsx
@@ -3,30 +3,10 @@ import Sheet from "@/components/Sheet";
 import { webResponseInitial } from "@/store/helpers";
 import { getPartners, partnersActions } from "@/store/partners";
 import { imageUrlFor } from "@/store/sanity";
+import theme from "@/utils/theme";
 import React from "react";
 import { connect } from "react-redux";
 import styled from "styled-components";
-
-const Wrapper = styled.div`
-  margin: 20px;
-  max-width: 1088px;
-  margin-left: auto;
-  margin-right: auto;
-`;
-
-const List = styled.ul``;
-
-const ListItem = styled(Sheet)`
-  margin: 20px 0;
-`;
-
-const Title = styled.h2``;
-
-const Description = styled(SanityBlockContent)``;
-
-const Image = styled.img`
-  max-width: 200px;
-`;
 
 const Partners = props => {
   const { partners } = props;
@@ -40,27 +20,30 @@ const Partners = props => {
     const partnerItems = partners.data
       .filter(partnerItem => partnerItem.type === partnerType)
       .map(partnerItem => (
-        <ListItem>
-          <Image
+        <Partner>
+          <PartnerImage
             src={imageUrlFor(partnerItem.image)
               .maxWidth(200)
               .url()}
             alt={partnerItem.name}
           />
-          <Title>{partnerItem.name}</Title>
-          <Description blocks={partnerItem.description} />
-        </ListItem>
+          <PartnerText>
+            <PartnerName>
+              <a href={partnerItem.partnerUrl}>{partnerItem.name}</a>
+            </PartnerName>
+            <SanityBlockContent blocks={partnerItem.description} />
+          </PartnerText>
+        </Partner>
       ));
-
     return <List>{partnerItems}</List>;
   };
 
   return (
     <Wrapper>
-      <h1>Partnere</h1>
-      <h2>Hovedpartnere</h2>
+      <PageTitle>Partnere</PageTitle>
+      <PageSubtitle>Hovedpartnere</PageSubtitle>
       <PartnerList partnerType="mainpartner" />
-      <h2>Partnere</h2>
+      <PageSubtitle>Partnere</PageSubtitle>
       <PartnerList partnerType="partner" />
     </Wrapper>
   );
@@ -92,3 +75,55 @@ export default connect(
   mapStateToProps,
   mapDispatchToProps
 )(Partners);
+
+const Wrapper = styled.div`
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+`;
+
+const PageTitle = styled.h1`
+  color: ${theme.purple};
+  text-transform: uppercase;
+  font-size: 40px;
+`;
+
+const PageSubtitle = styled.h2`
+  color: ${theme.darkgray};
+  text-transform: uppercase;
+  text-align: center;
+  font-size: 20px;
+`;
+
+const List = styled.ul`
+  padding: 0;
+`;
+
+const Partner = styled(Sheet)`
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  max-width: 1088px;
+  margin: 20px 0;
+
+  @media (min-width: 800px) {
+    flex-direction: row;
+    justify-content: space-around;
+  }
+`;
+
+const PartnerImage = styled.img`
+  max-width: 150px;
+  margin: 20px;
+`;
+
+const PartnerText = styled.div`
+  max-width: 700px;
+`;
+
+const PartnerName = styled.h3`
+  a {
+    text-decoration: none;
+    color: ${theme.purple};
+  }
+`;
