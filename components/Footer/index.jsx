e54ef5207fb24eb2cfc2cebae752a956993210ef
kariesta <kariesta@ifi.uio.no>
2019-03-22 01:21:29 +0100

    Initial footer

---
+++ components/Footer/index.jsx
@@ -0,0 +1,58 @@
+//import Button from "@/components/Button";
+import theme from "@/utils/theme";
+//import { faChevronDown, faChevronUp } from "@fortawesome/free-solid-svg-icons";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import Link from "next/link";
+import { darken } from "polished";
+import React, { useState } from "react";
+import styled from "styled-components";
+//import Navigation from "./navigation";
+const Container = styled.footer`
+  background-color: white;
+  border-top: 3px solid #ddd;
+  justify-content: space-around;
+  padding: 0 40px;
+  display: wrap;
+`;
+
+const Footer = () => {
+  return (
+    <Container>
+      <div>
+        <h3>Oslo Pride AS</h3>
+        <p>c/o Foreningen FRI<br/> Tollbugata 24<br/> 0157 OSLO</p>
+        <p>Tlf: 915 44 090</p>
+        <p>Org.nr: 986 625 860</p>
+      </div>
+      <div>
+        <Link href="/about">
+          <h3>Om oss</h3>
+        </Link>
+        <Link href="/contact">
+        <p>Kontakt</p>
+        </Link>
+        <Link href="/">
+        <p>Partnere-kommer snart</p>
+        </Link>
+      </div>
+      <div>
+      <Link href="https://www.facebook.com/oslopride/">
+        <h3>Følg oss</h3>
+      </Link>
+      <Link href="https://www.facebook.com/oslopride/">
+        <p>Facebook</p>
+      </Link>
+      <Link href="https://www.instagram.com/oslopride/">
+        <p>Instagram</p>
+      </Link>
+      </div>
+      <div>
+      <h3>INTERPRIDE AGM 2020</h3>
+      <p>mer info kommer snart</p>
+      </div>
+    </Container>
+
+  )
+}
+
+export default Footer;
