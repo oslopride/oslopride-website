1edd0ef07f592d9e375feec759c22d25390dbb76
Boye Borg <boye.borg@icloud.com>
2019-03-22 08:42:39 +0100

    update with orgnr and tlf links

--- components/Footer/index.jsx
+++ components/Footer/index.jsx
@@ -3,6 +3,64 @@ import Link from "next/link";
 import React from "react";
 import styled from "styled-components";
 
+const Footer = () => (
+  <Wrapper>
+    <Container>
+      <LinkWrapper>
+        <div>
+          <Link href="/about" passHref>
+            <FooterLinks>
+              <h3>OM OSS</h3>
+            </FooterLinks>
+          </Link>
+          <Link href="/contact" passHref>
+            <FooterLinks>Kontakt</FooterLinks>
+          </Link>
+          <Link href="/partners" passHref>
+            <FooterLinks>Partnere</FooterLinks>
+          </Link>
+        </div>
+        <div>
+          <FooterLinks href="https://www.facebook.com/oslopride/">
+            <h3>FØLG OSS</h3>
+          </FooterLinks>
+          <FooterLinks href="https://www.facebook.com/oslopride/">
+            Facebook
+          </FooterLinks>
+          <FooterLinks href="https://www.instagram.com/oslopride/">
+            Instagram
+          </FooterLinks>
+        </div>
+        <Link href="/interpride2020" passHref>
+          <FooterLinks>
+            <h3>INTERPRIDE AGM 2020</h3>
+          </FooterLinks>
+        </Link>
+      </LinkWrapper>
+      <OrgInfo>
+        <h3>OSLO PRIDE AS</h3>
+        <p>
+          c/o Foreningen FRI
+          <br />
+          Tollbugata 24
+          <br />
+          0157 OSLO
+        </p>
+        <p>
+          <TlfOrgNrLink href="tel:915 44 090">Tlf: 915 44 090</TlfOrgNrLink>
+        </p>
+        <p>
+          <TlfOrgNrLink href="https://w2.brreg.no/enhet/sok/detalj.jsp?orgnr=986625860">
+            Org.nr: 986 625 860
+          </TlfOrgNrLink>
+        </p>
+      </OrgInfo>
+    </Container>
+  </Wrapper>
+);
+
+export default Footer;
+
 const Wrapper = styled.footer`
   background-color: ${theme.blue};
   color: white;
@@ -34,8 +92,6 @@ const LinkWrapper = styled.div`
   }
 `;
 
-const LinkSection = styled.div``;
-
 const FooterLinks = styled.a`
   color: inherit;
   text-decoration: none;
@@ -47,54 +103,12 @@ const FooterLinks = styled.a`
   }
 `;
 
-const Footer = () => (
-  <Wrapper>
-    <Container>
-      <LinkWrapper>
-        <LinkSection>
-          <Link href="/about" passHref>
-            <FooterLinks>
-              <h3>OM OSS</h3>
-            </FooterLinks>
-          </Link>
-          <Link href="/contact" passHref>
-            <FooterLinks>Kontakt</FooterLinks>
-          </Link>
-          <Link href="/partners" passHref>
-            <FooterLinks>Partnere</FooterLinks>
-          </Link>
-        </LinkSection>
-        <LinkSection>
-          <FooterLinks href="https://www.facebook.com/oslopride/">
-            <h3>FØLG OSS</h3>
-          </FooterLinks>
-          <FooterLinks href="https://www.facebook.com/oslopride/">
-            Facebook
-          </FooterLinks>
-          <FooterLinks href="https://www.instagram.com/oslopride/">
-            Instagram
-          </FooterLinks>
-        </LinkSection>
-        <Link href="/interpride2020" passHref>
-          <FooterLinks>
-            <h3>INTERPRIDE AGM 2020</h3>
-          </FooterLinks>
-        </Link>
-      </LinkWrapper>
-      <OrgInfo>
-        <h3>OSLO PRIDE AS</h3>
-        <p>
-          c/o Foreningen FRI
-          <br />
-          Tollbugata 24
-          <br />
-          0157 OSLO
-        </p>
-        <p>Tlf: 915 44 090</p>
-        <p>Org.nr: 986 625 860</p>
-      </OrgInfo>
-    </Container>
-  </Wrapper>
-);
+const TlfOrgNrLink = styled.a`
+  color: inherit;
+  text-decoration: none;
 
-export default Footer;
+  :hover,
+  :focus {
+    text-decoration: underline;
+  }
+`;
