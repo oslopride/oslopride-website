2dc72c2f49c50f279c4234d1470d18eafc4c0a94
kariesta <kariesta@ifi.uio.no>
2019-03-22 01:23:36 +0100

    Initial footer

--- components/Footer/index.jsx
+++ components/Footer/index.jsx
@@ -1,12 +1,7 @@
-//import Button from "@/components/Button";
-import theme from "@/utils/theme";
-//import { faChevronDown, faChevronUp } from "@fortawesome/free-solid-svg-icons";
-import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import Link from "next/link";
-import { darken } from "polished";
-import React, { useState } from "react";
+import React from "react";
 import styled from "styled-components";
-//import Navigation from "./navigation";
+
 const Container = styled.footer`
   background-color: white;
   border-top: 3px solid #ddd;
@@ -20,7 +15,13 @@ const Footer = () => {
     <Container>
       <div>
         <h3>Oslo Pride AS</h3>
-        <p>c/o Foreningen FRI<br/> Tollbugata 24<br/> 0157 OSLO</p>
+        <p>
+          c/o Foreningen FRI
+          <br />
+          Tollbugata 24
+          <br />
+          0157 OSLO
+        </p>
         <p>Tlf: 915 44 090</p>
         <p>Org.nr: 986 625 860</p>
       </div>
@@ -29,30 +30,29 @@ const Footer = () => {
           <h3>Om oss</h3>
         </Link>
         <Link href="/contact">
-        <p>Kontakt</p>
+          <p>Kontakt</p>
         </Link>
         <Link href="/">
-        <p>Partnere-kommer snart</p>
+          <p>Partnere-kommer snart</p>
         </Link>
       </div>
       <div>
-      <Link href="https://www.facebook.com/oslopride/">
-        <h3>Følg oss</h3>
-      </Link>
-      <Link href="https://www.facebook.com/oslopride/">
-        <p>Facebook</p>
-      </Link>
-      <Link href="https://www.instagram.com/oslopride/">
-        <p>Instagram</p>
-      </Link>
+        <Link href="https://www.facebook.com/oslopride/">
+          <h3>Følg oss</h3>
+        </Link>
+        <Link href="https://www.facebook.com/oslopride/">
+          <p>Facebook</p>
+        </Link>
+        <Link href="https://www.instagram.com/oslopride/">
+          <p>Instagram</p>
+        </Link>
       </div>
       <div>
-      <h3>INTERPRIDE AGM 2020</h3>
-      <p>mer info kommer snart</p>
+        <h3>INTERPRIDE AGM 2020</h3>
+        <p>mer info kommer snart</p>
       </div>
     </Container>
-
-  )
-}
+  );
+};
 
 export default Footer;
