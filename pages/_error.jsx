9784f2728bfdd72b08689fec0ffdafad54149ca4
Boye Borg <boye.borg@icloud.com>
2019-03-27 07:33:58 +0100

    imporve error page

--- pages/_error.jsx
+++ pages/_error.jsx
@@ -9,39 +9,40 @@ const Wrapper = styled(Sheet)`
   }
 `;
 
-const is400Error = statusCode => statusCode >= 400 && statusCode < 500;
-
 const Error = ({ statusCode }) => {
-  if (is400Error(statusCode)) {
-    return (
-      <Wrapper>
-        <h1>Siden du leter etter finnes ikke</h1>
-        <p>
-          Vi har lett i alle kriker og kroker, men kan ikke finne siden du leter
-          etter.
-        </p>
-        <NextSeo
-          config={{
-            title: "Siden finnes ikke",
-            noindex: true
-          }}
-        />
-      </Wrapper>
-    );
+  switch (statusCode) {
+    case 404:
+      return (
+        <Wrapper>
+          <h1>Siden du leter etter finnes ikke</h1>
+          <p>
+            Vi har lett i alle kriker og kroker, men kan ikke finne siden du
+            leter etter.
+          </p>
+          <NextSeo
+            config={{
+              title: "Siden finnes ikke",
+              noindex: true
+            }}
+          />
+        </Wrapper>
+      );
+    default:
+      return (
+        <Wrapper>
+          <h1>Noe er riv ruskende galt...</h1>
+          <p>
+            Vi beklager, men det du forsøkte på fungerte rett og slett ikke.
+          </p>
+          <NextSeo
+            config={{
+              title: "Noe gikk galt",
+              noindex: true
+            }}
+          />
+        </Wrapper>
+      );
   }
-
-  return (
-    <Wrapper>
-      <h1>Noe er riv ruskende galt...</h1>
-      <p>Vi beklager, men det du forsøkte på fungerte rett og slett ikke.</p>
-      <NextSeo
-        config={{
-          title: "Noe gikk galt",
-          noindex: true
-        }}
-      />
-    </Wrapper>
-  );
 };
 
 Error.getInitialProps = async ({ res, err, isServer }) => {
