d379ea3c46e31673645d852ec128114aedf1f258
Siddise Hirpa <siddisehirpa@gmail.com>
2019-04-28 14:11:19 +0200

    connected store to events frontend

--- pages/program/index.jsx
+++ pages/program/index.jsx
@@ -1,44 +1,70 @@
 import Sheet from "@/components/Sheet";
+import { webResponseInitial } from "@/store/helpers";
+import { eventsActions, getEvents } from "@/store/program";
 import NextSeo from "next-seo";
 import React from "react";
+import { connect } from "react-redux";
 import styled from "styled-components";
 
-const Wrapper = styled.div`
+const Wrapper = styled(Sheet)`
   width: 100%;
-
-  @media (min-width: 1000px) {
-    width: 1000px;
-  }
+  max-width: 1000px;
 `;
 
-const Program = () => (
-  <Sheet>
-    <Wrapper>
-      <h1>Program 2019</h1>
-      <p>Programmet for 2019 kommer snart.</p>
-    </Wrapper>
+const Program = props => {
+  const { events } = props;
 
-    <NextSeo
-      config={{
-        title: "Festivalprogram",
-        description:
-          "Festivalprogram til Norges største feiring av skeiv kjærlighet og mangfold.",
-        openGraph: {
-          type: "website",
-          url: "https://oslopride.no/program",
-          locale: "nb_NO",
-          site_name: "Oslo Pride",
-          title: "Oslo Pride Festivalprogram",
+  if (events.status !== "SUCCESS") {
+    // TODO: Make a better UX while loading
+    return <div>Laster ...</div>;
+  }
+
+  return (
+    <>
+      <Wrapper>
+        <h1>Program 2019</h1>
+      </Wrapper>
+
+      <NextSeo
+        config={{
+          title: "Festivalprogram",
           description:
             "Festivalprogram til Norges største feiring av skeiv kjærlighet og mangfold.",
-          images: [
-            { url: "https://oslopride.no/static/logo.jpg" },
-            { url: "https://oslopride.no/static/prideheart.jpg" }
-          ]
-        }
-      }}
-    />
-  </Sheet>
-);
+          openGraph: {
+            type: "website",
+            url: "https://oslopride.no/program",
+            locale: "nb_NO",
+            site_name: "Oslo Pride",
+            title: "Oslo Pride Festivalprogram",
+            description:
+              "Festivalprogram til Norges største feiring av skeiv kjærlighet og mangfold.",
+            images: [
+              { url: "https://oslopride.no/static/logo.jpg" },
+              { url: "https://oslopride.no/static/prideheart.jpg" }
+            ]
+          }
+        }}
+      />
+    </>
+  );
+};
 
-export default Program;
+Program.getInitialProps = async ({ store, isServer }) => {
+  if (store.getState().events.status === webResponseInitial().status) {
+    store.dispatch(eventsActions.request());
+    if (isServer) {
+      try {
+        const response = await getEvents();
+        store.dispatch(eventsActions.success(response));
+      } catch (e) {
+        store.dispatch(eventsActions.failure(`${e}`));
+      }
+    }
+  }
+};
+
+const mapStateToProps = state => ({
+  events: state.events
+});
+
+export default connect(mapStateToProps)(Program);
