7d2f37aa1bec422716cf0e4c5e777a9687cbe621
Siddise Hirpa <siddisehirpa@gmail.com>
2019-05-07 18:15:29 +0200

    add next-seo for single event

--- pages/event/index.jsx
+++ pages/event/index.jsx
@@ -9,6 +9,7 @@ import {
 } from "@/store/helpers";
 import theme from "@/utils/theme";
 import dayjs from "dayjs";
+import NextSeo from "next-seo";
 import React from "react";
 import { connect } from "react-redux";
 import styled from "styled-components";
@@ -31,7 +32,8 @@ const Event = ({ event }) =>
         prices,
         ageLimit,
         deafInterpretation,
-        accessible
+        accessible,
+        _id
       } = e;
 
       const start = dayjs(startingTime);
@@ -39,80 +41,111 @@ const Event = ({ event }) =>
       const singleDayEvent = start.diff(end, "day") === 0;
 
       return (
-        <Sheet>
-          {singleDayEvent ? (
-            <>
-              <EventTimeDay>
-                <OrangeColor>{start.format("dddd")} </OrangeColor>
-                {start.format("D MMMM YYYY")}
-              </EventTimeDay>
-              <EventTimeFromTo>
-                {start.format("HH:mm")} - {end.format("HH:mm")}
-              </EventTimeFromTo>
-            </>
-          ) : (
-            <>
-              <EventTimeDay>
-                <EventTimeLabel>Fra </EventTimeLabel>
-                <OrangeColor>{start.format("dddd")} </OrangeColor>
-                {start.format("D MMMM YYYY")}
-                <MultidayEventTime> {start.format("HH:mm")}</MultidayEventTime>
-              </EventTimeDay>
-              <EventTimeDay>
-                <EventTimeLabel>Til </EventTimeLabel>
-                <OrangeColor>{end.format("dddd")} </OrangeColor>
-                {end.format("D MMMM YYYY")}
-                <MultidayEventTime> {end.format("HH:mm")}</MultidayEventTime>
-              </EventTimeDay>
-            </>
-          )}
-          <Title>{title}</Title>
-          <SanityBlockContent blocks={description} />
-          <Details>
-            <OrangeColor as="h2">Detaljer</OrangeColor>
-            <div>
-              <strong>Arrangør: </strong>
-              {organizer}
-            </div>
-            <div>
-              <strong>Sted: </strong>
-              {location.name && `${location.name}, `}
-              {location.address}
-            </div>
-            <div>
-              <strong>Tidspunkt: </strong>
-              {singleDayEvent
-                ? `${start.format("HH:mm")} - ${end.format("HH:mm")}`
-                : `${start.format("D MMMM HH:mm")} - ${end.format(
-                    "D MMMM HH:mm"
-                  )}`}
-            </div>
-            <div>
-              <strong>Pris: </strong>
-              {free
-                ? "Gratis"
-                : prices === undefined
-                ? "Ukjent"
-                : prices
-                    .map(
-                      ({ amount, priceLabel }) => `${amount} (${priceLabel})`
-                    )
-                    .join(", ")}
-            </div>
-            <div>
-              <strong>Aldersgrense: </strong>
-              {ageLimit === "0" ? "Fri aldersgrense" : `${ageLimit} år`}
-            </div>
-            <div>
-              <strong>Tegnspråktolket: </strong>
-              {deafInterpretation ? "Ja" : "Nei"}
-            </div>
-            <div>
-              <strong>Universell utforming: </strong>
-              {accessible ? "Ja" : "Nei"}
-            </div>
-          </Details>
-        </Sheet>
+        <>
+          <Sheet>
+            {singleDayEvent ? (
+              <>
+                <EventTimeDay>
+                  <OrangeColor>{start.format("dddd")} </OrangeColor>
+                  {start.format("D. MMMM YYYY")}
+                </EventTimeDay>
+                <EventTimeFromTo>
+                  {start.format("HH:mm")} - {end.format("HH:mm")}
+                </EventTimeFromTo>
+              </>
+            ) : (
+              <>
+                <EventTimeDay>
+                  <EventTimeLabel>Fra </EventTimeLabel>
+                  <OrangeColor>{start.format("dddd")} </OrangeColor>
+                  {start.format("D. MMMM YYYY")}
+                  <MultidayEventTime>
+                    {" "}
+                    {start.format("HH:mm")}
+                  </MultidayEventTime>
+                </EventTimeDay>
+                <EventTimeDay>
+                  <EventTimeLabel>Til </EventTimeLabel>
+                  <OrangeColor>{end.format("dddd")} </OrangeColor>
+                  {end.format("D. MMMM YYYY")}
+                  <MultidayEventTime> {end.format("HH:mm")}</MultidayEventTime>
+                </EventTimeDay>
+              </>
+            )}
+            <Title>{title}</Title>
+            <SanityBlockContent blocks={description} />
+            <Details>
+              <OrangeColor as="h2">Detaljer</OrangeColor>
+              <div>
+                <strong>Arrangør: </strong>
+                {organizer}
+              </div>
+              <div>
+                <strong>Sted: </strong>
+                {location.name && `${location.name}, `}
+                {location.address}
+              </div>
+              <div>
+                <strong>Tidspunkt: </strong>
+                {singleDayEvent
+                  ? `${start.format("HH:mm")} - ${end.format("HH:mm")}`
+                  : `${start.format("D. MMMM HH:mm")} - ${end.format(
+                      "D MMMM HH:mm"
+                    )}`}
+              </div>
+              <div>
+                <strong>Pris: </strong>
+                {free
+                  ? "Gratis"
+                  : prices === undefined
+                  ? "Ukjent"
+                  : prices
+                      .map(
+                        ({ amount, priceLabel }) => `${amount} (${priceLabel})`
+                      )
+                      .join(", ")}
+              </div>
+              <div>
+                <strong>Aldersgrense: </strong>
+                {ageLimit === "0" ? "Fri aldersgrense" : `${ageLimit} år`}
+              </div>
+              <div>
+                <strong>Tegnspråktolket: </strong>
+                {deafInterpretation ? "Ja" : "Nei"}
+              </div>
+              <div>
+                <strong>Universell utforming: </strong>
+                {accessible ? "Ja" : "Nei"}
+              </div>
+            </Details>
+          </Sheet>
+
+          <NextSeo
+            config={{
+              title: title,
+              description: `${start.format(
+                "dddd D. MMMM YYYY HH:mm"
+              )} - ${end.format("HH:mm")} @ ${location.name &&
+                location.name + ", "}${location.address}`,
+              openGraph: {
+                type: "website",
+                url: `https://oslopride.no/events/${_id}`,
+                title,
+                locale: "nb_NO",
+                site_name: "Oslo Pride",
+                title: title,
+                description: `${start.format(
+                  "dddd D. MMMM YYYY HH:mm"
+                )} - ${end.format("HH:mm")} @ ${location.name &&
+                  location.name + ", "}${location.address}`,
+                images: [
+                  { url: "https://oslopride.no/static/logo.jpg" },
+                  { url: "https://oslopride.no/static/prideheart.jpg" }
+                ]
+              }
+            }}
+          />
+        </>
       );
     }
   });
