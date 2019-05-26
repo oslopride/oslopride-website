cf147f72d7b19907a05251b5f8c0a297bffddc33
Boye Borg <boye.borg@icloud.com>
2019-05-26 20:11:38 +0200

    add polyfills

--- next.config.js
+++ next.config.js
@@ -1,5 +1,19 @@
 const withCSS = require("@zeit/next-css");
 
 module.exports = withCSS({
-  target: "serverless"
+  target: "serverless",
+  webpack: function(cfg) {
+    const originalEntry = cfg.entry;
+    cfg.entry = async () => {
+      const entries = await originalEntry();
+      if (
+        entries["main.js"] &&
+        !entries["main.js"].includes("./polyfills.js")
+      ) {
+        entries["main.js"].unshift("./polyfills.js");
+      }
+      return entries;
+    };
+    return cfg;
+  }
 });
---
+++ polyfills.js
@@ -0,0 +1,3 @@
+/* eslint no-extend-native: 0 */
+// core-js comes with Next.js. So, you can import it like below
+import "core-js/features/array/flat";
