327b0cdf47cafef2ef9288a08e4bbc57487af919
Siddise Hirpa <siddisehirpa@gmail.com>
2019-05-26 14:37:24 +0200

    add link, description and subtitle to event headliners

--- components/FeaturedDates/index.jsx
+++ components/FeaturedDates/index.jsx
@@ -1,3 +1,4 @@
+import Link from "@/components/Link";
 import React from "react";
 import styled from "styled-components";
 
@@ -18,19 +19,26 @@ const FeaturedDates = ({ dates }) => {
         <h2>Gå til programmet</h2>
       </TitleWrapper>
       <div>
-        {dates.map(({ _key: key, date, title }, index) => (
-          <DateWrapper key={key}>
-            <DateTime>{date}</DateTime>
-            <div>
-              <DateTitle
-                color={DateTitleColors[index % DateTitleColors.length]}
-              >
-                {title}
-              </DateTitle>
-              <div>Konserter, debatter, utsttillinger og fester!</div>
-            </div>
-          </DateWrapper>
-        ))}
+        {dates.map(
+          ({ _key: key, date, title, description, subtitle, link }, index) => (
+            <DateWrapper key={key} href={link}>
+              <DateTime>{date}</DateTime>
+              <div>
+                <DateTitle
+                  color={DateTitleColors[index % DateTitleColors.length]}
+                >
+                  {title}
+                </DateTitle>
+                <DateDescription>{description}</DateDescription>
+                <DateSubtitle
+                  color={DateTitleColors[index % DateTitleColors.length]}
+                >
+                  {subtitle}
+                </DateSubtitle>
+              </div>
+            </DateWrapper>
+          )
+        )}
       </div>
     </>
   );
@@ -44,9 +52,16 @@ const TitleWrapper = styled.div`
   justify-content: space-between;
 `;
 
-const DateWrapper = styled.div`
+const DateWrapper = styled(Link)`
   text-align: center;
-  margin-bottom: 50px;
+  transition: all 0.2s ease-in-out;
+  box-shadow: none;
+
+  :hover,
+  :focus {
+    transform: scale(1.05);
+    box-shadow: none;
+  }
 
   @media (min-width: 1025px) {
     display: flex;
@@ -63,17 +78,38 @@ const DateWrapper = styled.div`
 
 const DateTitle = styled.div`
   font-size: 40px;
+  text-align: center;
   font-weight: bolder;
   text-transform: uppercase;
   color: ${props => props.color};
-  
+
   @media (min-width: 1025px) {
     display: inline-block;
     font-size: 50px;
+  }
 `;
 
-const DateTime = styled.span`
+const DateTime = styled.div`
   font-size: 20px;
   font-weight: 600;
   margin-right: 20px;
+`;
+
+const DateDescription = styled.p`
+  font-size: 18px;
+  margin: 0 auto;
+  max-width: 350px;
+`;
+
+const DateSubtitle = styled.p`
+  font-size: 18px;
+  font-weight: 600;
+  margin: 0 auto;
+  margin-bottom: 50px;
+  text-transform: uppercase;
+  color: ${props => props.color};
+
+  @media (min-width: 1025px) {
+    margin-bottom: 0;
+  }
 `;
