29f3e3558eb64531449196d9fbe5c866a80efcd1
Boye Borg <boye.borg@icloud.com>
2019-05-28 18:14:44 +0200

    improve toggle

--- components/Filter/Toggles.jsx
+++ components/Filter/Toggles.jsx
@@ -4,22 +4,25 @@ import React from "react";
 import { FaToggleOff, FaToggleOn } from "react-icons/fa";
 import styled from "styled-components";
 
-const Toggles = ({ toggles, className }) => {
-  return (
-    <Container className={className}>
-      {toggles.map(({ on, off, isOn, callback }) => (
-        <ToggleWrapper key={`toggle-${off}-${on}`}>
-          <ToggleText gray={isOn}>{off}</ToggleText>
-          <Toggle isOn={isOn} callback={callback} />
-          <ToggleText gray={!isOn}>{on}</ToggleText>
-        </ToggleWrapper>
-      ))}
-    </Container>
-  );
-};
+const Toggles = ({ toggles, className }) => (
+  <Container className={className}>
+    {toggles.map(({ on, off, isOn, callback }) => (
+      <ToggleWrapper key={`toggle-${off}-${on}`}>
+        <ToggleText onClick={isOn ? callback : undefined} gray={isOn}>
+          {off}
+        </ToggleText>
+        <Toggle isOn={isOn} callback={callback} />
+        <ToggleText onClick={isOn ? undefined : callback} gray={!isOn}>
+          {on}
+        </ToggleText>
+      </ToggleWrapper>
+    ))}
+  </Container>
+);
 
 const ToggleText = styled.span`
   color: ${({ gray }) => (gray ? "gray" : "black")};
+  cursor: pointer;
 `;
 
 const Toggle = styled(Switch)`
