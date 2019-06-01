82bc74b9cee262a6ab7fb25aa3f5bd26b907cad6
Boye Borg <boye.borg@icloud.com>
2019-06-01 19:04:41 +0200

    make hero not suck (#152)

---
+++ components/Hero/index.jsx
@@ -0,0 +1,93 @@
+import React from "react";
+import styled from "styled-components";
+import Card from "./card";
+import Slider from "react-slick";
+
+const settings = {
+  autoplay: true,
+  autoplaySpeed: 5000,
+  pauseOnHover: true,
+  centerMode: true,
+  infinite: true,
+  arrows: false,
+  dots: true,
+  slidesToShow: 1,
+  speed: 500
+};
+
+const Hero = () => (
+  <Wrapper>
+    <Overlay />
+    <Image src="https://images.ctfassets.net/r522rjz18n3u/58XT8KdYIwCSeAiQQ24YwO/1757190cacc1c0571a5bf17308e841c3/47744330842_635b05fc0e_o.jpg" />
+    <CardWrapper {...settings}>
+      <Card
+        title="Oslo Pride"
+        body="Norges største feiring av skeiv kjærlighet og mangfold"
+        index={0}
+      />
+      <Card
+        title="Oslo Pride"
+        body="Norges største feiring av skeiv kjærlighet og mangfold"
+        index={1}
+      />
+      <Card
+        title="Oslo Pride"
+        body="Norges største feiring av skeiv kjærlighet og mangfold"
+        index={2}
+      />
+      <Card
+        title="Oslo Pride"
+        body="Norges største feiring av skeiv kjærlighet og mangfold"
+        index={3}
+      />
+      <Card
+        title="Oslo Pride"
+        body="Norges største feiring av skeiv kjærlighet og mangfold"
+        index={4}
+      />
+      <Card
+        title="Oslo Pride"
+        body="Norges største feiring av skeiv kjærlighet og mangfold"
+        index={5}
+      />
+    </CardWrapper>
+  </Wrapper>
+);
+
+const Wrapper = styled.div`
+  position: relative;
+  height: 80vh;
+  width: 100%;
+  margin-top: -70px;
+  display: flex;
+  flex-direction: column;
+  margin-bottom: 50px;
+`;
+
+const Image = styled.img`
+  width: 100%;
+  height: 80vh;
+  object-fit: cover;
+`;
+
+const CardWrapper = styled(Slider)`
+  width: 100%;
+  position: absolute;
+  bottom: calc(40vh - 200px);
+`;
+
+const Overlay = styled.div`
+  position: absolute;
+  z-index: 500;
+  top: 0;
+  left: 0;
+  right: 0;
+  height: 150px;
+  background-image: linear-gradient(
+    to bottom,
+    rgba(255, 255, 255, 0.8) 0%,
+    rgba(255, 255, 255, 0) 150px
+  );
+`;
+
+export default Hero;
