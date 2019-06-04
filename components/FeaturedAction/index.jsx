60cc3add876bc29c9b962d0dccf3c31df1ff9f2a
Siddise Hirpa <siddisehirpa@gmail.com>
2019-06-04 19:54:40 +0200

    add empty featuredaction component

---
+++ components/FeaturedAction/index.jsx
@@ -0,0 +1,17 @@
+import React from "react";
+import styled from "styled-components";
+
+const FeaturedActions = () => {
+  return (
+    <Wrapper>
+      <div />
+      <img />
+    </Wrapper>
+  );
+};
+
+export default FeaturedActions;
+
+const Wrapper = styled.div``;
+
+const Image = styled.img``;
