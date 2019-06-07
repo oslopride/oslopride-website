fea9ad71dbd25052893d72b829ef66cc88fb46b7
Siddise Hirpa <siddisehirpa@gmail.com>
2019-06-07 23:17:42 +0200

    add utc to event page

--- pages/event/index.jsx
+++ pages/event/index.jsx
@@ -41,8 +41,8 @@ const Event = ({ event }) =>
         eventWebpage
       } = e;
 
-      const start = dayjs(startingTime).add(2, "hour");
-      const end = dayjs(endingTime).add(2, "hour");
+      const start = dayjs.utc(startingTime).add(2, "hour");
+      const end = dayjs.utc(endingTime).add(2, "hour");
       const singleDayEvent = start.diff(end, "day") === 0;
 
       return (
