82660bfa00143327b6b126ed5d3ad2670f889fa2
Siddise Hirpa <siddisehirpa@gmail.com>
2019-06-09 15:46:48 +0200

    add component to import

--- pages/index.jsx
+++ pages/index.jsx
@@ -1,5 +1,6 @@
 import ArticlePreview from "@/components/ArticlePreview";
 import Banner from "@/components/Banner";
+import FeaturedAction from "@/components/FeaturedAction";
 import FeaturedButtons from "@/components/FeaturedButtons";
 import FeaturedDates from "@/components/FeaturedDates";
 import FeaturedPartners from "@/components/FeaturedPartners";
